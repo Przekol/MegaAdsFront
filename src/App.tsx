9b728c681cb10024ab2b9cae93cd78d42a3662e3
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-02-02 08:43:50 +0100

    Basic structure of main view

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
 import React from 'react';
+import './App.css';
 
 export function App() {
-  return <p>Test..</p>;
+  return (
+    <>
+      <header>
+        <h1>
+          <strong>Mega </strong>Ogłoszenia
+        </h1>
+        <button>Dodaj ogłoszenie</button>
+        <div className="search">
+          <input type="text" /> <button>Szukaj</button>
+        </div>
+      </header>
+      <div className="map">..</div>
+    </>
+  );
 }
