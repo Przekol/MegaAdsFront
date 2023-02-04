2188145ad19c1d916a8cce3cacee29891d2fff86
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-02-04 13:37:43 +0100

    Add map to project using Leaflet library

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,6 +1,17 @@
 import * as React from 'react';
 import styles from './Map.module.css';
+import { MapContainer, TileLayer } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
 
 export const Map = () => {
-  return <div className={styles.map}>..</div>;
+  return (
+    <div className={styles.map}>
+      <MapContainer className={styles.leafletContainer} center={[50.2657152, 18.9945008]} zoom={13}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution="&copy; <a href='https://www.openstreetmap.org/opyright'>OpenStreetMap</a> & contributors"
+        />
+      </MapContainer>
+    </div>
+  );
 };
