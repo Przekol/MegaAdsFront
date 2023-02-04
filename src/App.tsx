210dcf416111f5531c8d0f26a2bf685da8eee5b9
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-02-04 13:02:51 +0100

    Fix: add wrapper and it's styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import styles from './App.module.css';
 
 import { Header } from './components/layout';
 import { Map } from './components/Map';
 
 export function App() {
   return (
-    <>
+    <div className={styles.wrapper}>
       <Header />
       <Map />
-    </>
+    </div>
   );
 }
