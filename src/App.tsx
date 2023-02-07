c3c72f0958ff1b290eefe536b7ca80fb9872e4af
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-02-07 10:04:24 +0100

    Add SearchContext to store and manage search value

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './App.module.css';
 
 import { Header } from './components/layout';
 import { Map } from './components/Map';
+import { SearchContext } from './context/search.context';
 
 export function App() {
+  const [search, setSearch] = useState('');
   return (
-    <div className={styles.wrapper}>
-      <Header />
-      <Map />
-    </div>
+    <SearchContext.Provider value={{ search, setSearch }}>
+      <div className={styles.wrapper}>
+        <Header />
+        <Map />
+      </div>
+    </SearchContext.Provider>
   );
 }
--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -3,10 +3,19 @@ import styles from './Map.module.css';
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 import '../../utils/fix-map-icon';
 import 'leaflet/dist/leaflet.css';
+import { useContext, useEffect } from 'react';
+import { SearchContext } from '../../context/search.context';
 
 export const Map = () => {
+  const { search } = useContext(SearchContext);
+
+  useEffect(() => {
+    console.log('Make request to search for', search);
+  }, [search]);
+
   return (
     <div className={styles.map}>
+      <h1>Search for: {search}</h1>
       <MapContainer className={styles.leafletContainer} center={[50.2657152, 18.9945008]} zoom={13}>
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
