3b023c830bd5b679f1b1b1fd4f14b607b8f9d92f
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-02-09 16:22:04 +0100

    Implementation of displaying searched ad

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -3,30 +3,40 @@ import styles from './Map.module.css';
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 import '../../utils/fix-map-icon';
 import 'leaflet/dist/leaflet.css';
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { SearchContext } from '../../context/search.context';
+import { GetListOfAdsResponse } from 'types';
+import { apiClient } from '../../servises/api';
+import { SingleAd } from './SingleAd';
 
 export const Map = () => {
   const { search } = useContext(SearchContext);
+  const [ads, setAds] = useState<GetListOfAdsResponse>([]);
 
   useEffect(() => {
-    console.log('Make request to search for', search);
+    (async () => {
+      const result = await apiClient.get<GetListOfAdsResponse>(`/ad/search/${search}`);
+
+      if (result.ok && result.data) {
+        setAds(result.data);
+      }
+    })();
   }, [search]);
 
   return (
     <div className={styles.map}>
-      <h1>Search for: {search}</h1>
       <MapContainer className={styles.leafletContainer} center={[50.2657152, 18.9945008]} zoom={13}>
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution="&copy; <a href='https://www.openstreetmap.org/opyright'>OpenStreetMap</a> & contributors"
         />
-        <Marker position={[50.2657152, 18.9945008]}>
-          <Popup>
-            <h2>IT.focus</h2>
-            <p>Super Firma programistyczna</p>
-          </Popup>
-        </Marker>
+        {ads.map((ad) => (
+          <Marker key={ad.id} position={[ad.lat, ad.lon]}>
+            <Popup>
+              <SingleAd id={ad.id} />
+            </Popup>
+          </Marker>
+        ))}
       </MapContainer>
     </div>
   );
