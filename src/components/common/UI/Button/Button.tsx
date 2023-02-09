342256acf5ba87f325794d0a53b484cfc3240441
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-02-09 20:33:21 +0100

    Styles for button-like links fixed

--- src/components/common/UI/Button/Button.tsx
+++ src/components/common/UI/Button/Button.tsx
@@ -1,12 +1,19 @@
 import * as React from 'react';
 import { ButtonHTMLAttributes, ReactNode } from 'react';
 import styles from './Button.module.css';
+import { Link } from 'react-router-dom';
 
 interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
+  to?: string;
 }
-export const Button = ({ children, ...rest }: Props) => (
-  <button className={styles.button} {...rest}>
-    {children}
-  </button>
-);
+export const Button = ({ children, to, ...rest }: Props) =>
+  to ? (
+    <Link className={styles.button} to={to}>
+      {children}
+    </Link>
+  ) : (
+    <button className={styles.button} {...rest}>
+      {children}
+    </button>
+  );
